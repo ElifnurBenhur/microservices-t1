e9e6c6a2252d295e130230adcef474d9e1e2a719
Elifnur Benhür <elifnur.benhur@gmail.com>
2023-05-21 10:43:55 +0300

    payment service added

---
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/RentalServiceApplication.java
@@ -0,0 +1,15 @@
+package com.kodlamaio.rentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.Rental.ServiceBasePackage})
+public class RentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalServiceApplication.class, args);
+	}
+
+}
